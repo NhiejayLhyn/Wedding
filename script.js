f9b34153a04b24ba5a1d78dcadbcb26788d35fec
NhiejayLhyn <delacruz.mjade10@gmail.com>
2026-08-09 18:10:06 +0800

    Update script.js

--- script.js
+++ script.js
@@ -28,12 +28,12 @@ const WEDDING = {
    --------------------------------------------------------- */
 const GALLERY = [
   { src: "assets/couple-photo.jpg", alt: "Niejhay and Lhyn smiling together" }
-  { src: "gallery/1.jpg", alt: "Niejhay and Lhyn smiling together" }
-  { src: "gallery/2.jpg", alt: "Niejhay and Lhyn smiling together" }
-  { src: "gallery/3.jpg", alt: "Niejhay and Lhyn smiling together" }
-  { src: "gallery/4.jpg", alt: "Niejhay and Lhyn smiling together" }
-  { src: "gallery/5.jpg", alt: "Niejhay and Lhyn smiling together" }
-  { src: "gallery/6.jpg", alt: "Niejhay and Lhyn smiling together" }
+  { src: "assets/gallery/1.jpg", alt: "Niejhay and Lhyn smiling together" }
+  { src: "assets/gallery/2.jpg", alt: "Niejhay and Lhyn smiling together" }
+  { src: "assets/gallery/3.jpg", alt: "Niejhay and Lhyn smiling together" }
+  { src: "assets/gallery/4.jpg", alt: "Niejhay and Lhyn smiling together" }
+  { src: "assets/gallery/5.jpg", alt: "Niejhay and Lhyn smiling together" }
+  { src: "assets/gallery/6.jpg", alt: "Niejhay and Lhyn smiling together" }
 // { src: "assets/gallery/photo-2.jpg", alt: "Describe this photo" },
 ];
 
